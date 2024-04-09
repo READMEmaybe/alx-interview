24c81aeede5172b55177ad05e1f13e7b054f78f9
READMEmaybe <haroune11411@gmail.com>
2024-04-09 15:55:02 +0000

    fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,6 +2,8 @@
 
 def pascal_triangle(n):
     """Return a list of lists representing the Pascal's triangle of n rows."""
+    if n <= 0:
+        return []
     trngl = [[1]]
     for _ in range(1, n):
         row = [1] + [trngl[-1][i] + trngl[-1][i+1] for i in range(len(trngl[-1]) - 1)] + [1]
