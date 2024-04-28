dbd2851bd6ee9b463876734403e7ba0c6c429ec7
READMEmaybe <haroune11411@gmail.com>
2024-04-29 00:41:59 +0100

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,10 @@
+def canUnlockAll(boxes):
+    """ Check if all boxes can be unlocked """
+    if len(boxes) == 0:
+        return False
+    keys = [0]
+    for key in keys:
+        for new_key in boxes[key]:
+            if new_key not in keys and new_key < len(boxes):
+                keys.append(new_key)
+    return len(keys) == len(boxes)
